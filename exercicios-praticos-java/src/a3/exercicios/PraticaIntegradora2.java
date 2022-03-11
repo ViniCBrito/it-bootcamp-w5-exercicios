e885f7a505be304145fb9f86d3a1ac42fde62396
Vinicius Clemente Brito <vinicius.brito@mercadolivre.com>
2022-03-11 16:05:28 -0300

    Ajuste no método main de execução.

--- exercicios-praticos-java/src/a3/exercicios/PraticaIntegradora2.java
+++
@@ -1,7 +0,0 @@
-package a3.exercicios;
-
-public class PraticaIntegradora2 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ exercicios-praticos-java/src/a3/exercicios/PraticaIntegradora2/PraticaIntegradora2.java
@@ -0,0 +1,11 @@
+package a3.exercicios.PraticaIntegradora2;
+
+import a3.exercicios.PraticaIntegradora2.Model.ParticipanteModel;
+import a3.exercicios.PraticaIntegradora2.View.AppView;
+
+public class PraticaIntegradora2 {
+    public static void main(String[] args) {
+        AppView app = new AppView();
+        app.start();
+    }
+}
